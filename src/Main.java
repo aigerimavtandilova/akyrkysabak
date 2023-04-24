5bff3bc6dae0b1afb467ba98209b6dd61bd63f13
Aigerim <aygerim083@gmail.com>
2023-04-24 16:39:05 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+    String[]a=new String[]{"Mugalim", "İt","Vrach"};
+    method(a);
+
+
+    }
+    public static void method(String [] kesip){
+        for (String b:kesip) {
+            System.out.println("Menin kesibim:"+b);
+        }
+}}
